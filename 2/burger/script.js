580660da5460e5ba9fa9583b8e92b19b8024c5af
Ilya G <ilya.croworg@gmail.com>
2022-11-12 21:33:57 +0300

    Lesson 2. Переключился на третий урок.

---
+++ 2/burger/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// class Burger {
+//     constructor() {
+//         const ingridients = {
+//             size: {},
+//             stuffing: {},
+//             options: {}
+//         }
+//     }
+//
+// }
